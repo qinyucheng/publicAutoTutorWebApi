568000aeb460581d26e687579cf11c9f842d1ebf
Yehui Liu <yehuiliu1@gmail.com>
2016-03-08 20:06:56 -0600

    putwholeClassInfo

---
+++ Models/CreateClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace publicAutoTutorWebApi.Models
+{
+    public class CreateClass
+    {
+        public List<string> StudentsName { get; set; }
+    }
+
+}
